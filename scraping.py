dbacf8a899166537697878ca89a85ae9ee582312
da-isaac <ibjorgen97@gmail.com>
2022-07-10 14:18:20 -0600

    Updated scraping file with functions

--- scraping.py
+++ scraping.py
@@ -1,115 +1,83 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-# import Splinter and BeautifulSoup
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
 
+def scrape_all():
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+    news_title, news_paragraph = mars_news(browser)
 
-# In[2]:
+    data = {
+        'news_title': news_title,
+        'news_paragraph': news_paragraph,
+        'featured_image': featured_image(browser),
+        'facts': mars_facts(),
+        'last_modified': dt.datetime.now()
+    }
 
+    browser.quit()
+    return data
 
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+def mars_news(browser):
 
+    # Visit Mars NASA news site
+    url = 'https://redplanetscience.com'
+    browser.visit(url)
 
-# In[3]:
+    # Optional delay for loading the page
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
 
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
 
-# Visit Mars NASA news site
-url = 'https://redplanetscience.com'
-browser.visit(url)
+    try:
+        slide_elem = news_soup.select_one('div.list_text')
+        news_title = slide_elem.find('div', class_='content_title').get_text()
+        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+    except AttributeError:
+        return None, None
+    
+    return news_title, news_p
 
-# Optional delay for loading the page
-browser.is_element_present_by_css('div.list_text', wait_time=1)
+def featured_image(browser):
 
+    # Visit URL
+    url = 'https://spaceimages-mars.com'
+    browser.visit(url)
 
-# In[4]:
+    # Find and click the full image button
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
 
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
 
-html = browser.html
-news_soup = soup(html, 'html.parser')
-slide_elem = news_soup.select_one('div.list_text')
+    try:
+        # Find the relative image url
+        img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+    
+    except AttributeError:
+        return None
+    
+    # Use base url to create absolute url
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
 
+    return img_url
 
-# In[5]:
+def mars_facts():
+    try:
+        df = pd.read_html('https://galaxyfacts-mars.com')[0]
+    
+    except BaseException:
+        return None
+    
+    df.columns=['description', 'Mars', 'Earth']
+    df.set_index('description', inplace=True)
 
+    return df.to_html()
 
-news_title = slide_elem.find('div', class_='content_title').get_text()
-news_title
-
-
-# In[6]:
-
-
-news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-news_p
-
-
-# ### Featured Images
-
-# In[7]:
-
-
-# Visit URL
-url = 'https://spaceimages-mars.com'
-browser.visit(url)
-
-
-# In[8]:
-
-
-# Find and click the full image button
-full_image_elem = browser.find_by_tag('button')[1]
-full_image_elem.click()
-
-
-# In[9]:
-
-
-# Parse the resulting html with soup
-html = browser.html
-img_soup = soup(html, 'html.parser')
-
-
-# In[10]:
-
-
-# Find the relative image url
-img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
-img_url_rel
-
-
-# In[11]:
-
-
-# Use base url to create absolute url
-img_url = f'https://spaceimages-mars.com/{img_url_rel}'
-img_url
-
-
-# In[13]:
-
-
-df = pd.read_html('https://galaxyfacts-mars.com')[0]
-df.columns=['description', 'Mars', 'Earth']
-df.set_index('description', inplace=True)
-df
-
-
-# In[14]:
-
-
-df.to_html()
-
-
-# In[15]:
-
-
-browser.quit()
-
+if __name__ == '__main__':
+    print(scrape_all())
